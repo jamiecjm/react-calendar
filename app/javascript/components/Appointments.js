47b8ed7d5504cc700cacc30da8e40965b2cfb32d
jiemeechong <jamiecjm@gmail.com>
2018-05-26 11:33:55 +1000

    Initial commit

---
+++ app/javascript/components/Appointments.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Appointments extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>React Calendar</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Appointments
