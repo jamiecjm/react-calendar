9e591f787e17af0cb11b4b3805eeafb5e8ce697f
jiemeechong <jamiecjm@gmail.com>
2018-05-26 11:56:03 +1000

    Building appointment form with jquery and haml

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'webpacker'
 gem 'react-rails'
 gem 'haml'
+gem 'jquery-rails'
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,19 @@
 class AppointmentsController < ApplicationController
+
   def index
+    @appointments = Appointment.order('appt_time ASC')
+    @appointment = Appointment.new
   end
+
+  def create
+    @appointments = Appointment.order('appt_time ASC')
+    @appointment = Appointment.create(appointment_params)
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appt_time, :title)
+  end
+
 end
