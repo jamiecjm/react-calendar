d4d2c12ee0d603665589a5f9a50512ca06436701
jiemeechong <jamiecjm@gmail.com>
2018-05-26 13:38:52 +1000

    Handling user form input

--- app/javascript/components/AppointmentForm.js
+++ app/javascript/components/AppointmentForm.js
@@ -2,14 +2,26 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class AppointmentForm extends React.Component {
+
+  handleChange(e){
+    var name = e.target.name;
+    var obj = {};
+    obj['input_'+name] = e.target.value;
+    this.props.onInput(obj);
+  }
+
   render () {
     return (
       <React.Fragment>
         <div>
           <h2>Make an new appointment</h2>
           <form>
-            <input name='title' placeholder='Appointment Title'/>
-            <input name='appt_time' placeholder='Date and Time'/>
+            <input name='title' placeholder='Appointment Title'
+            value={this.props.input_title}
+            onChange={this.handleChange.bind(this)}/>
+            <input name='appt_time' placeholder='Date and Time'
+            value={this.props.input_appt_time}
+            onChange={this.handleChange.bind(this)}/>
             <input type='submit' value='Make Appointment'/>
           </form>
         </div>
--- app/javascript/components/Appointments.js
+++ app/javascript/components/Appointments.js
@@ -4,11 +4,27 @@ import AppointmentForm from './AppointmentForm'
 import AppointmentList from './AppointmentList'
 
 class Appointments extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        appointments: this.props.appointments,
+        input_title: 'Meet Joe',
+        input_appt_time: 'Tomorrow at 9am'
+      };
+  }
+
+  handleUserInput(obj){
+    this.setState(obj)
+  }
+
   render () {
     return (
       <React.Fragment>
-        <AppointmentForm/>
-        <AppointmentList appointments={this.props.appointments}/>
+        <AppointmentForm input_title={this.state.input_title}
+        input_appt_time={this.state.input_appt_time}
+        onInput={this.handleUserInput.bind(this)}/>
+        <AppointmentList appointments={this.state.appointments}/>
       </React.Fragment>
     );
   }
